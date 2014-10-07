fcf1d85915d70fea9fd84521d3137512dd7cc38b
Juan Herrera <jok3rcit0>
2014-10-06 19:02:02 -0500

    gulp para quitar coffe y mover a bin

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+//llamar a los modulos
+var gulp = require('gulp');
+var args = require('yargs').argv;
+var uglify = require('gulp-uglifyjs');
+var coffee = require('gulp-coffee');
+
+var activity = args.activity;
+
+function main(activity){
+	gulp.src([activity+'/js/main.coffee',])
+	.pipe(coffee({bare: true}))
+	.pipe(uglify())
+	.pipe(gulp.dest(activity+'/js/'));
+}
+
+function deal(activity){
+	gulp.src([activity+'/js/lib/dealersjs.coffee'])
+	.pipe(coffee({bare: true}))
+	.pipe(uglify('dealersjs.min.js'))
+	.pipe(gulp.dest(activity+'/js/lib/'))
+}
+
+gulp.task('production', function(){
+	deal(activity);
+	main(activity);
+	gulp.src([
+		activity+'/assets/**/*.*',
+		activity+'/css/**/*.*',
+		activity+'/fonts/**/*.*',
+		activity+'/js/**/*.js',
+		activity+'/js/**/*.min.js',
+		activity+'/index.html',
+		], {base: './'})
+	.pipe(gulp.dest('Bin'));
+	console.log('Activity '+activity+' Complete!');
+});
